984ba2852d191d1604408476785b8d053b05c5cd
Vitor Senise <vitor.senise@gmail.com>
2020-08-06 12:58:01 -0300

    Initial Setup from Day 4

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Image, Text } from 'react-native'
+
+import './styles'
+
+function Landing() {
+  return (
+    <View>
+      <Text>Hello, Mobile!</Text>
+    </View>
+  )
+}
+
+export default Landing
